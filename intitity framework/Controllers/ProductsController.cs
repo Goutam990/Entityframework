27ece7b6787fd28f1f022b7b7fd3a87fa5a8accc
Goutam Tiwari <tiwarigoutam8@gmail.com>
2025-09-18 22:33:19 +0530

    Add project files.

---
+++ intitity framework/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using intitity_framework.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace intitity_framework.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var products = _context.Products.ToList();
+            return View(products);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
